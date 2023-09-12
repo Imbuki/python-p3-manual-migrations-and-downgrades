fcbf323da054586b312f360b8a114898197e6a7f
Imbuki <+>
2023-09-12 22:46:03 +0300

    t
    done

--- lib/migrations/versions/6b9cb35ba46e_empty_init.py
+++ lib/migrations/versions/6b9cb35ba46e_empty_init.py
@@ -1,8 +1,8 @@
-"""Empty Init
+"""Renaming enrolled_date column to start_date
 
-Revision ID: 6b9cb35ba46e
-Revises: 
-Create Date: 2022-08-04 13:21:26.936909
+Revision ID: c1c936c797a9
+Revises: 791279dd0760
+Create Date: 2023-02-09 14:38:21.671362
 
 """
 from alembic import op
@@ -10,15 +10,17 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '6b9cb35ba46e'
-down_revision = None
+revision = 'c1c936c797a9'
+down_revision = '791279dd0760'
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
     pass
+    op.alter_column('students', 'enrolled_date', new_column_name='start_date')
 
 
 def downgrade() -> None:
     pass
+    op.alter_column('students', 'start_date', new_column_name='enrolled_date')
